e259b0557d5c86d572c3036aca0437614881a4d0
joefdavis4444 <joseph.davis@techconsulting.tech>
2025-12-07 21:27:00 -0500

    Update nfl_receiving_inc.py

--- Incremental/nfl_receiving_inc.py
+++ Incremental/nfl_receiving_inc.py
@@ -12,9 +12,9 @@ spark = SparkSession.builder \
 spark.sql("USE joepostgres")
 
 # --- Configuration ---
-bronze_table = "nfl_receiving"         # Target Table (Used for COUNT CHECKPOINT - stores RAW data)
-inc_source_table = "nfl_receiving_inc" # Source Table (New Raw Data)
-silver_path = "hdfs:///tmp/DE011025/Joe/silver/nfl_receiving_output" # Final Output Path for Cleaned Data
+bronze_table = "nfl_receiving"       
+inc_source_table = "nfl_receiving_inc" 
+silver_path = "hdfs:///tmp/DE011025/Joe/silver/nfl_receiving_output" 
 
 # Load current target count (nfl_receiving)
 # This will load the raw data from the last run to check its count
